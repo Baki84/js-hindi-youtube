5c4b7f70a8ee3cb459aa0942d64a56a184352748
shashank shah <110417736+Baki84@users.noreply.github.com>
2026-02-14 08:22:42 +0000

    Added 03_conversionOperation.js for type conversion practice

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,24 @@
+let score = "33abc"
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log( valueInNumber);
+
+
+//"33" => 33
+//"33abc" => NaN
+//true/false => 1/0
+
+// "" => false
+//"shashank" => true
+let isLoggedIn = 1
+
+let booleanisLoggedIn = Boolean(isLoggedIn)
+console.log(booleanisLoggedIn)
+
+
+let someNumber = 33
+let StringNumber =  String(someNumber)
+console.log( typeof StringNumber);
