78ed0754b9d47c4a6b8cc84665c6f5627780507c
shashank shah <110417736+Baki84@users.noreply.github.com>
2026-02-14 09:11:47 +0000

    Added 05_strings.js with string methods practice

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,25 @@
+const name = "hitesh"
+const repoCount = 50
+
+//console.log(name + repoCount+ "value");
+
+console.log(`Hello my name is
+     ${name} and my repo count is ${repoCount}`)
+
+    //  const gameName = new String('hitesh')
+    //  console.log(gameName[0]);
+    //  console.log(gameName.length);
+    //  console.log(gameName.toUpperCase());
+//     const gameName = "Shashank"
+
+// const st = gameName.substring(0,4)
+// console.log(st)
+
+const newStringone = "  chaii  "
+const qw = (newStringone.trim());
+console.log(newStringone);
+
+console.log(qw);
+
+     
+     
