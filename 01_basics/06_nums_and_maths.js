e043ed89a585ad5812de2b62c0aba2beb1682a08
shashank shah <110417736+Baki84@users.noreply.github.com>
2026-02-14 09:33:28 +0000

    Added 06_nums_and_maths.js with number and math methods practice

---
+++ 01_basics/06_nums_and_maths.js
@@ -0,0 +1,19 @@
+// const score = 400
+// console.log( score);
+
+// const balance = new Number(100)
+// console.log(balance);
+
+// console.log(balance.toString().length);
+
+// console.log(balance.toFixed(2));
+// const otherNumber = 123.8966
+// console.log(otherNumber.toPrecision(3));
+
+// const hundred = 1000000
+// console.log(hundred.toLocaleString('en-IN'))
+
+//+++++++++++++MATHS++++++++++++++++++
+
+console.log(Math.abs(-4));
+console.log(Math.round(4.6));
