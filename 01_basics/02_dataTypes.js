7935c6670998dcbaf84c60400f22d4f0e10eb841
shashank shah <110417736+Baki84@users.noreply.github.com>
2026-02-14 06:57:47 +0000

    Added 02_dataTypes.js

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,11 @@
+//alert(3+3)  //we are using node js , not browser
+let name  = "hitesh"
+let age = 18
+let isLoggedIn = false
+
+//number => 2 topower 53
+//bigint 
+//String =>""
+//boolean => true/false
+// null => standalone value
+//undefined => 
